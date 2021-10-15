3506cf1a05136396f6cab7bce7c3effea8e125c0
IvanM <ivan.malyshev.376@gmail.com>
2021-10-15 20:44:01 +0300

    network layers

--- index.js
+++ index.js
@@ -46,74 +46,112 @@ const calculateInput = (result, weight) => {
   }
 };
 
-const network = [];
-const init = (l1, l2) => {
-  // console.log('init', l1, l2);
-  const layerWeights = [];
-  for (let i = 0; i < l1; i++) {
-    layerWeights[i] = [];
-    for (let j = 0; j < l2; j++) {
-      layerWeights[i].push(round(Math.random()));
-    }
-  }
-  network.push(layerWeights);
-};
 const config = { layers: [2, 3, 2] };
 const { layers } = config;
-layers.forEach((i, index) => layers[index + 1] && init(layers[index], layers[index + 1]));
+let weightId = 0; // weights count
+let neuronId = 0;
+const network = [];
+layers.forEach((neuronsCountCurrentLayer, index) => {
+  const neuronsCountPrevLayer = layers[index - 1];
+  const neuronsCountNextLayer = layers[index + 1];
+  const layer = [];
+  for (let i = 0; i < neuronsCountCurrentLayer; i++) {
+    const neuron = { input: [], output: [], neuronId: neuronId++, layerId: index };
+    if (neuronsCountPrevLayer) {
+      network[index - 1].forEach(neuronPrev => {
+        neuronPrev.output.forEach((weight, weightIndex) => {
+          i === weightIndex && neuron.input.push(weight);
+        });
+      });
+    }
+    if (neuronsCountNextLayer) {
+      for (let i = 0; i < neuronsCountNextLayer; i++) {
+        const weight = { value: round(Math.random()), id: weightId++ };
+        neuron.output.push(weight);
+      }
+    }
+    layer.push(neuron);
+  }
+  network.push(layer);
+});
+const printNetwork = () => {
+  const layerMap = neuron => ({
+    id: neuron.neuronId,
+    layer: `L${neuron.layerId}`,
+    input: neuron.input.map(weight => `id:${weight.id} value:${weight.value}`).join(' | '),
+    value: neuron.value || 0,
+    expected: neuron.expected || 0, // expected value
+    output: neuron.output.map(weight => `id:${weight.id} value:${weight.value}`).join('\t| '),
+  });
+  console.table(network.reduce((a, b) => a.concat(b)).map(layerMap));
+};
+// printNetwork();debugger;
 
-console.log('NETWORK:', network.map(layer => JSON.stringify(layer)).join('\n'));
+const run = (input, expected) => {
+  network[0].forEach((neuron, index) => (neuron.value = input[index])); // set initial input
 
-const run = (train, weights_old, initialInput, expected = []) => {
-  const values = [initialInput]; // intermediate projected values - set initial input
+  // PROJECT VALUES:
   network.forEach((layer, layerIndex) => {
-    const input = values[layerIndex];
-    const valuesNext = [];
-    values.push(valuesNext); // set next values layer
-
-    const projectedWeights = [];
-    debugger;
+    const nextLayer = network[layerIndex + 1] || [];
     layer.forEach((neuron, neuronIndex) => {
-      projectedWeights.push(neuron.map(weight => round(projectWeight(input[neuronIndex], weight))));
-    });
-    debugger;
-
-    // for (let i = 0; i<projectedWeights.length; i+=)
-    projectedWeights.forEach(inputResults => {
-      inputResults.forEach((result, index) => {
-        if (!valuesNext[index]) {
-          valuesNext[index] = result;
-        } else {
-          valuesNext[index] += result;
-        }
-        // if (index === 0) {
-        //   console.log(111, result, valuesNext[index]);
-        // }
+      neuron.output.forEach((weight, weightIndex) => {
+        // console.log(round(projectWeight(neuron.value, weight.value)));
+        nextLayer[weightIndex].value += projectWeight(neuron.value, weight.value);
+        nextLayer[weightIndex].value = round(nextLayer[weightIndex].value);
       });
     });
-    //
-    console.log(`projectedWeights ${JSON.stringify(projectedWeights)}`);
-    // console.log(values[layerIndex].length, layer.length);
-    // console.log(`values1 ${JSON.stringify(values)}`);
-    valuesNext = valuesNext.map(i => {
-      return round(i / (values[layerIndex].length * layer.length));
-    });
-    // console.log(`values2 ${JSON.stringify(values)}`);
+    nextLayer.forEach(neuron => (neuron.value = round(neuron.value / layer.length)));
   });
-  console.log(`values:${JSON.stringify(values)}`);
-  // process.exit();
 
-  const [i1, i2] = input;
-  const [w1, w2, w3, w4, w5, w6, w7, w8] = weights;
+  // TRAIN:
+  if (expected) {
+    printNetwork();
+    debugger;
+    network[network.length - 1].forEach((neuron, index) => (neuron.expected = expected[index]));
+    printNetwork();
+    debugger;
+    for (let layerIndex = network.length - 1; layerIndex >= 0; layerIndex--) {
+      console.log(layerIndex);
+      const layer = network[layerIndex];
+      const prevLayer = network[layerIndex - 1] || [];
+      console.log(layerIndex, layer, prevLayer);
+      layer.forEach((neuron, neuronIndex) => {
+        neuron.output.forEach((weight, weightIndex) => {
+          // console.log(round(projectWeight(neuron.value, weight.value)));
+          prevLayer[weightIndex].value += projectWeight(neuron.value, weight.value);
+          nextLayer[weightIndex].value = round(nextLayer[weightIndex].value);
+        });
+      });
 
-  const n1 = round((projectWeight(i1, w1) + projectWeight(i2, w3)) / 2);
-  const n2 = round((projectWeight(i1, w2) + projectWeight(i2, w4)) / 2);
+      adjustWeight(weight, result < expectedResult[resultIndex]);
+    }
+    printNetwork();
+    debugger;
+    ///
 
-  const r1 = round((projectWeight(n1, w5) + projectWeight(n2, w7)) / 2);
-  const r2 = round((projectWeight(n1, w6) + projectWeight(n2, w8)) / 2);
+    const valuesReversed = values.slice().reverse();
+    const networkReversed = JSON.parse(JSON.stringify(network)).slice().reverse();
+    const networkReversedCopy = JSON.parse(JSON.stringify(networkReversed));
+    let expectedResult = expected;
+    valuesReversed.forEach((results, reversedIndex) => {
+      results.forEach((result, resultIndex) => {
+        networkReversedCopy[reversedIndex][resultIndex] = networkReversed[reversedIndex][resultIndex].map(weight =>
+          adjustWeight(weight, result < expectedResult[resultIndex])
+        );
+        debugger;
+      });
+    });
+  }
+  debugger;
 
-  let logString = `input:[${i1},${i2}] result:[${r1},${r2}]`;
-  let logWeights = `weights:\t${weights.join('\t')}`;
+  // const [i1, i2] = input;
+  // const [w1, w2, w3, w4, w5, w6, w7, w8] = weights;
+  // const n1 = round((projectWeight(i1, w1) + projectWeight(i2, w3)) / 2);
+  // const n2 = round((projectWeight(i1, w2) + projectWeight(i2, w4)) / 2);
+  // const r1 = round((projectWeight(n1, w5) + projectWeight(n2, w7)) / 2);
+  // const r2 = round((projectWeight(n1, w6) + projectWeight(n2, w8)) / 2);
+  // let logString = `input:[${i1},${i2}] result:[${r1},${r2}]`;
+  // let logWeights = `weights:\t${weights.join('\t')}`;
   if (train) {
     const updatedWeights = weights.slice();
     const [e1, e2] = expected;
@@ -134,24 +172,23 @@ const run = (train, weights_old, initialInput, expected = []) => {
     updatedWeights[3] = adjustWeight(w4, n2 < newN2);
 
     logWeights += `\nupdatedWeights:\t${updatedWeights.join('\t')}`;
-    // setTimeout(() => run(0, updatedWeights, input), 100);
+    // setTimeout(() => run(input), 100);
     return updatedWeights;
   }
   console.log(`${logString}\n${logWeights}`);
 };
 
 const setExpected = x => round((Math.random() + x) / 2);
-run(0, weights, [0.2, 0.7]);
-for (let i = 0; i < 333333; i++) {
-  // for (let i = 0; i < 9999; i++) {
+// run([0.2, 0.7]);
+for (let i = 0; i < 3; i++) {
   let expected = [Math.floor(Math.random() * 2)]; // config expected result
   expected[1] = expected[0] === 0 ? 1 : 0;
   const input = [setExpected(expected[0]), setExpected(expected[1])];
-  weights = run(1, weights, input, expected);
+  weights = run(input, expected);
   // console.log(input, expected);
   // console.log(weights.join('\t'));
 }
-run(0, weights, [0.2, 0.7]); // check
+run([0.2, 0.7]); // check
 
 // adjustWeight(0.69, 0, 0.5, 0.77);
 // const propagated_error = weight * weightsDelta;
